f3517032b1ebaef88333b9d57185014a908f157a
Leandro <45185345+Leodolz@users.noreply.github.com>
2020-03-04 11:09:24 -0400

    Add files via upload

--- Validators.cs
+++ Validators.cs
@@ -7,17 +7,17 @@ namespace AgendaApp
     public static class Validators
     {
         private static string[] localeDateFormats = CultureInfo.CurrentCulture.DateTimeFormat.GetAllDateTimePatterns();
-        public static bool isValidDate(string dateEntry)
+        public static bool IsValidDate(string dateEntry)
         {
             DateTime parsedDate;
             return DateTime.TryParseExact(dateEntry, localeDateFormats, CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedDate);
         }
-        public static bool isValidTime(string timeEntry)
+        public static bool IsValidTime(string timeEntry)
         {
             Regex timeChecker = new Regex(@"^(?i)(0?[1-9]|1[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?( AM| PM)?$");
             return timeChecker.IsMatch(timeEntry);
         }
-        public static DateTime getDateTime(string dateEntry)
+        public static DateTime GetDateTime(string dateEntry)
         {
             DateTime parsedDate;
             DateTime.TryParseExact(dateEntry, localeDateFormats, CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedDate);
